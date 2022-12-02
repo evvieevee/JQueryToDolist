4683e91d163fdcae19d21755f0bf475927158eaa
evvieevee <eve.puurtinen00@gmail.com>
2022-12-02 18:25:20 +0200

    adds comments

--- ToDolist.js
+++ ToDolist.js
@@ -1,9 +1,10 @@
+// Alustetaan numerojärjestys taskilistalle.
 let activeNum = 0;
-
+// Tämä on taskin poisto-funktio. joka slideup animointi ja poista sitten elementin.
 function removeTask(id) {
   $( "#"+id ).slideUp( "slow", function() { $(this).remove() });
 }
-
+// Tämä on yliviivaus funktio, jolla merkataan taski tehdyksi tai poistetaan taskin kuittaus.
 function addDeco(id) {
   const textElement = $( "#"+id+ " p" );
   if (textElement.hasClass("text-decoration-line-through")) {
@@ -13,20 +14,23 @@ function addDeco(id) {
   }
   
 }
-
-
+// Tässä ei ole käytetty local storagea tai ulkoista databasea tietojen tallentamiseen. 
+// Tässä on sumbit, eli lisäys funktio. 
+// kun inputfieldiin on lisätty tekstiä ja tallenna nappia painetaan, tulee se listaksi alle. 
 $( "form" ).submit(function( event ) {
+// Estetään sivun refreshaus eli päivitys, jottei taskit tyhjene kesken kaiken.
     event.preventDefault();
     const inputValue = $( "input" ).first().val();
     activeNum++;
+    // Tässä määritellään poistonapille tyylit ja teksti. 
+    // Tehdään onclick event, jolle annetaan funktio, jolle annetaan parametriksi taskin id, jonka perusteella voimme löytää taskin. 
     $( `
-    <li id="${activeNum}" class="list-group-item d-flex justify-content-between" onclick="addDeco(${activeNum})" style="display: none;">
-      
+    <li id="${activeNum}" class="list-group-item d-flex justify-content-between" onclick="addDeco(${activeNum})" style="display: none;"> 
       <p class="text-truncate w-80 m-2" >${inputValue}</p>
-      <div class="d-flex">
-        <button type="button" class="btn btn-outline-dark" onclick="removeTask(${activeNum})">
-          Poista
-        </button>
-      </div>
-    </li>` ).appendTo($("ul")).slideDown("slow");
+      <button type="button" class="btn btn-outline-dark" onclick="removeTask(${activeNum})">
+        Poista
+      </button>
+    </li>` )
+    // Tässä tehdään slidedown animointi, joka tulee taskeille, kun ne lisätään sivulle (listaan)
+    .appendTo($("ul")).slideDown("slow");
 });
